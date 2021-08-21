5803ed1e5a18975d1e9b289045b5fe0c2c9a68f7
Janek <jan.skiba.poczta@gmail.com>
2021-08-21 18:52:12 +0200

    add slide out menu animation

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,12 @@
 const toggleMenu = () => {
   const icon = document.getElementById('nav-icon');
   const nav = document.querySelector('nav');
+  const content = document.querySelector('main');
 
   icon.addEventListener('click', () => {
     nav.classList.toggle('active');
     icon.classList.toggle('open');
+    content.classList.toggle('shadow');
   });
 };
 
