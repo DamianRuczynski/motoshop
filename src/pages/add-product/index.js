4d6f4d639357384fb0733c2c8072cbabe71038a7
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2021-08-23 17:03:21 +0200

    add basic content to add-product page

--- src/pages/add-product/index.js
+++ src/pages/add-product/index.js
@@ -1,4 +1,18 @@
 import './index.scss';
-import toggleMenu from '../../js/menu.js';
+import ProductCard from '../../js/product-card.js';
+import toggleMenu from '../../js/menu';
 
 toggleMenu();
+
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
+const imageContainer = document.querySelector('.product-card__images');
+
+const productCard = new ProductCard(imageContainer.childElementCount);
+
+leftArrow.addEventListener('click', () =>
+  productCard.handleLeftArrowClick(imageContainer)
+);
+rightArrow.addEventListener('click', () =>
+  productCard.handleRightArrowClick(imageContainer)
+);
